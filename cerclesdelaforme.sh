59a6e0cf37bcce565c43bdde658c0312768f21a7
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-13 16:16:00 +0200

    style: remove unnecessary escape for builtin

--- cerclesdelaforme.sh
+++ cerclesdelaforme.sh
@@ -82,27 +82,27 @@ saintjacques-14eme@8 Rue Emile Dubois, 75014 Paris
 # colorder[4]=1
 # colorder[5]=1
 
-\echo 'BEGIN:VCALENDAR'
-\echo 'VERSION:2.0'
-\echo 'PRODID:thomasleplus'
-\echo 'CALSCALE:GREGORIAN'
-\echo 'BEGIN:VTIMEZONE'
-\echo 'TZID:Europe/Paris'
-\echo 'BEGIN:DAYLIGHT'
-\echo 'TZOFFSETFROM:+0100'
-\echo 'TZOFFSETTO:+0200'
-\echo 'TZNAME:GMT+2'
-\echo 'DTSTART:19700329T020000'
-\echo 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU'
-\echo 'END:DAYLIGHT'
-\echo 'BEGIN:STANDARD'
-\echo 'TZOFFSETFROM:+0200'
-\echo 'TZOFFSETTO:+0100'
-\echo 'TZNAME:GMT+1'
-\echo 'DTSTART:19701025T030000'
-\echo 'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU'
-\echo 'END:STANDARD'
-\echo 'END:VTIMEZONE'
+echo 'BEGIN:VCALENDAR'
+echo 'VERSION:2.0'
+echo 'PRODID:thomasleplus'
+echo 'CALSCALE:GREGORIAN'
+echo 'BEGIN:VTIMEZONE'
+echo 'TZID:Europe/Paris'
+echo 'BEGIN:DAYLIGHT'
+echo 'TZOFFSETFROM:+0100'
+echo 'TZOFFSETTO:+0200'
+echo 'TZNAME:GMT+2'
+echo 'DTSTART:19700329T020000'
+echo 'RRULE:FREQ=YEARLY;BYMONTH=3;BYDAY=-1SU'
+echo 'END:DAYLIGHT'
+echo 'BEGIN:STANDARD'
+echo 'TZOFFSETFROM:+0200'
+echo 'TZOFFSETTO:+0100'
+echo 'TZNAME:GMT+1'
+echo 'DTSTART:19701025T030000'
+echo 'RRULE:FREQ=YEARLY;BYMONTH=10;BYDAY=-1SU'
+echo 'END:STANDARD'
+echo 'END:VTIMEZONE'
 
 events=()
 page=0
@@ -111,8 +111,8 @@ while [ "${page}" -lt "${nbpage}" ]; do
 	response=$(\curl -fsSL -H 'Accept-Encoding: gzip, deflate, br, zstd' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'DNT: 1' -H 'Host: www.cerclesdelaforme.com' -H 'Origin: https://www.cerclesdelaforme.com' -H 'Pragma: no-cache' -H 'Priority: u=0' -H 'Referer: https://www.cerclesdelaforme.com/horaires/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' -H 'TE: trailers' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:138.0) Gecko/20100101 Firefox/138.0' -H 'X-Requested-With: XMLHttpRequest' --data-raw "action=search_event&adulte=1&enfant=0&active%5B%5D=2489&active%5B%5D=257&active%5B%5D=2980&active%5B%5D=2490&active%5B%5D=3442&active%5B%5D=204&active%5B%5D=16709&active%5B%5D=273&active%5B%5D=8789&active%5B%5D=17540&active%5B%5D=275&active%5B%5D=15657&active%5B%5D=274&active%5B%5D=8790&active%5B%5D=262&active%5B%5D=260&active%5B%5D=2488&active%5B%5D=250&active%5B%5D=252&active%5B%5D=2487&active%5B%5D=136454&active%5B%5D=8471&active%5B%5D=6686&active%5B%5D=3226&active%5B%5D=3508&active%5B%5D=263&active%5B%5D=16702&active%5B%5D=2511&active%5B%5D=2985&active%5B%5D=256&active%5B%5D=3446&active%5B%5D=3440&active%5B%5D=267&active%5B%5D=2507&active%5B%5D=3221&start=7&end=22&page=${page}&type=main&colorder%5B1%5D=1&colorder%5B2%5D=1&colorder%5B3%5D=1&colorder%5B4%5D=1&colorder%5B5%5D=1" -X POST 'https://www.cerclesdelaforme.com/wp-admin/admin-ajax.php' -o - | gunzip)
 	while read -r event; do
 		events+=("${event}")
-	done < <(\echo "${response}" | \jq -c '.list[]')
-	nbpage=$(\echo "${response}" | \jq '.nbpage')
+	done < <(echo "${response}" | \jq -c '.list[]')
+	nbpage=$(echo "${response}" | \jq '.nbpage')
 	page=$((page + 1))
 done
 
@@ -120,7 +120,7 @@ IFS=','
 joined="${events[*]+"${events[*]}"}"
 IFS=$'\n\t'
 
-\echo '[' "${joined}" ']' | \jq -c 'sort_by(.eve_id) | .[]' | while read -r event; do
+echo '[' "${joined}" ']' | \jq -c 'sort_by(.eve_id) | .[]' | while read -r event; do
 	titre=$(echo "${event}" | \jq -r '.titre')
 	club_id=$(echo "${event}" | \jq -r '.club_id')
 	eve_id=$(echo "${event}" | \jq -r '.eve_id')
@@ -130,19 +130,19 @@ IFS=$'\n\t'
 	m_start=$(echo "${event}" | \jq -r '.m_start')
 	duree=$(echo "${event}" | \jq -r '.duree')
 	salle=$(echo "${event}" | \jq -r '.salle')
-	\echo 'BEGIN:VEVENT'
-	\echo "UID:C${club_id}E${eve_id}"
-	\echo "SUMMARY:${titre} @ ${club_code}"
-	\echo "DESCRIPTION:${salle}"
-	\echo "LOCATION:$(\echo "${ADDRESSES}" | \grep -e "${club_code}@" | sed -e 's/.*@//')"
-	\echo "DTSTAMP:19700101T000000Z"
-	\echo -n "DTSTART;TZID=Europe/Paris:${DATES[${jour}]}T"
-	\printf "%02d" "${h_start}"
-	\printf "%02d" "${m_start}"
-	\echo '00'
-	\echo "DURATION:PT${duree}M"
-	\echo "RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=${DAYS[${jour}]}"
-	\echo 'END:VEVENT'
+	echo 'BEGIN:VEVENT'
+	echo "UID:C${club_id}E${eve_id}"
+	echo "SUMMARY:${titre} @ ${club_code}"
+	echo "DESCRIPTION:${salle}"
+	echo "LOCATION:$(echo "${ADDRESSES}" | \grep -e "${club_code}@" | sed -e 's/.*@//')"
+	echo "DTSTAMP:19700101T000000Z"
+	echo -n "DTSTART;TZID=Europe/Paris:${DATES[${jour}]}T"
+	printf "%02d" "${h_start}"
+	printf "%02d" "${m_start}"
+	echo '00'
+	echo "DURATION:PT${duree}M"
+	echo "RRULE:FREQ=WEEKLY;INTERVAL=1;BYDAY=${DAYS[${jour}]}"
+	echo 'END:VEVENT'
 done
 
-\echo 'END:VCALENDAR'
+echo 'END:VCALENDAR'
