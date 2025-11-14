83a29a8b9c9b4016fe1d69ad03bd54e8c29f9f50
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-11-14 19:30:51 +0100

    fix(bash): no need for echo

--- cerclesdelaforme.sh
+++ cerclesdelaforme.sh
@@ -111,8 +111,8 @@ while [ "${page}" -lt "${nbpage}" ]; do
 	response=$(\curl -fsSL -H 'Accept-Encoding: gzip, deflate, br, zstd' -H 'Accept-Language: en-US,en;q=0.5' -H 'Accept: */*' -H 'Cache-Control: no-cache' -H 'Connection: keep-alive' -H 'Content-Type: application/x-www-form-urlencoded; charset=UTF-8' -H 'DNT: 1' -H 'Host: www.cerclesdelaforme.com' -H 'Origin: https://www.cerclesdelaforme.com' -H 'Pragma: no-cache' -H 'Priority: u=0' -H 'Referer: https://www.cerclesdelaforme.com/horaires/' -H 'Sec-Fetch-Dest: empty' -H 'Sec-Fetch-Mode: cors' -H 'Sec-Fetch-Site: same-origin' -H 'Sec-GPC: 1' -H 'TE: trailers' -H 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:138.0) Gecko/20100101 Firefox/138.0' -H 'X-Requested-With: XMLHttpRequest' --data-raw "action=search_event&adulte=1&enfant=0&active%5B%5D=2489&active%5B%5D=257&active%5B%5D=2980&active%5B%5D=2490&active%5B%5D=3442&active%5B%5D=204&active%5B%5D=16709&active%5B%5D=273&active%5B%5D=8789&active%5B%5D=17540&active%5B%5D=275&active%5B%5D=15657&active%5B%5D=274&active%5B%5D=8790&active%5B%5D=262&active%5B%5D=260&active%5B%5D=2488&active%5B%5D=250&active%5B%5D=252&active%5B%5D=2487&active%5B%5D=136454&active%5B%5D=8471&active%5B%5D=6686&active%5B%5D=3226&active%5B%5D=3508&active%5B%5D=263&active%5B%5D=16702&active%5B%5D=2511&active%5B%5D=2985&active%5B%5D=256&active%5B%5D=3446&active%5B%5D=3440&active%5B%5D=267&active%5B%5D=2507&active%5B%5D=3221&start=7&end=22&page=${page}&type=main&colorder%5B1%5D=1&colorder%5B2%5D=1&colorder%5B3%5D=1&colorder%5B4%5D=1&colorder%5B5%5D=1" -X POST 'https://www.cerclesdelaforme.com/wp-admin/admin-ajax.php' -o - | gunzip)
 	while read -r event; do
 		events+=("${event}")
-	done < <(\jq -c '.list[]' <<< "${response}")
-	nbpage=$(\jq '.nbpage' <<< "${response}")
+	done < <(\jq -c '.list[]' <<<"${response}")
+	nbpage=$(\jq '.nbpage' <<<"${response}")
 	page=$((page + 1))
 done
 
@@ -120,21 +120,21 @@ IFS=','
 joined="${events[*]+"${events[*]}"}"
 IFS=$'\n\t'
 
-\jq -c 'sort_by(.eve_id) | .[]' <<< "[${joined}]" | while read -r event; do
-	titre=$(\jq -r '.titre' <<< "${event}")
-	club_id=$(\jq -r '.club_id' <<< "${event}")
-	eve_id=$(\jq -r '.eve_id' <<< "${event}")
-	club_code=$(\jq -r '.club_code' <<< "${event}")
-	jour=$(\jq -r '.jour' <<< "${event}")
-	h_start=$(\jq -r '.h_start' <<< "${event}")
-	m_start=$(\jq -r '.m_start' <<< "${event}")
-	duree=$(\jq -r '.duree' <<< "${event}")
-	salle=$(\jq -r '.salle' <<< "${event}")
+\jq -c 'sort_by(.eve_id) | .[]' <<<"[${joined}]" | while read -r event; do
+	titre=$(\jq -r '.titre' <<<"${event}")
+	club_id=$(\jq -r '.club_id' <<<"${event}")
+	eve_id=$(\jq -r '.eve_id' <<<"${event}")
+	club_code=$(\jq -r '.club_code' <<<"${event}")
+	jour=$(\jq -r '.jour' <<<"${event}")
+	h_start=$(\jq -r '.h_start' <<<"${event}")
+	m_start=$(\jq -r '.m_start' <<<"${event}")
+	duree=$(\jq -r '.duree' <<<"${event}")
+	salle=$(\jq -r '.salle' <<<"${event}")
 	echo 'BEGIN:VEVENT'
 	echo "UID:C${club_id}E${eve_id}"
 	echo "SUMMARY:${titre} @ ${club_code}"
 	echo "DESCRIPTION:${salle}"
-	echo "LOCATION:$(\grep -e "${club_code}@" <<< "${ADDRESSES}" | sed -e 's/.*@//')"
+	echo "LOCATION:$(\grep -e "${club_code}@" <<<"${ADDRESSES}" | sed -e 's/.*@//')"
 	echo "DTSTAMP:19700101T000000Z"
 	echo -n "DTSTART;TZID=Europe/Paris:${DATES[${jour}]}T"
 	printf "%02d" "${h_start}"
